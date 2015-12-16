9b67cd3ec138417e914279943be884e0595af3e6
Paul Miller <jettero@corky.vhb.voltar.org>
2015-12-16 10:52:01 -0500

    this might be right... probably not, but maybe

---
+++ nbayes.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+class instance():
+    def __init__(self, label, *attr):
+        self.label = label
+        self.attr  = attr
+
+class classifier():
+    def __init__(self):
+        self.corpus = []
+
+    def add_instance(self, label='unknown', *attr):
+        corpus.append( instance(label, attr) )
+
+    def prob_label(self,label):
+        if not self.corpus:
+            return 0
+
+        c = 0
+        for i in self.corpus:
+            if i.label == label:
+                c += 1
+        return float(c) / len(self.corpus)
+
+    def prob_attr(self,*attr):
+        if not self.corpus:
+            return 0
+
+        p = []
+        for a in attr:
+            c = 0
+            for i in self.corpus:
+                if a in i.attr:
+                    c += 1
+            p.append( float(c) / len(self.corpus) )
+        return reduce(lambda a,b: a*b, p)
+
+    def prob_attr_given_label(self, label, *attr):
+        c = 0
+        for i in self.corpus:
+            if i.label == label:
+                if not set(attr) - set(i.attr): 
+                    c += 1
+        return float(c) / len( self.corpus )
+
+    def prob_label_given_attr(self, label, *attr):
+        # P(A|B) = P(B|A)*P(A) / P(B)
+        return (self.prob_attr_given_label(label,*attr) * self.prob_label(label)) / self.prob_attr(*attr)
