9cedaa63784f739c6d69034e7378e8b49794a1da
Paul Miller <jettero@corky.vhb.voltar.org>
2018-05-23 12:14:39 -0400

    add a few more working modes and another example with cards

---
+++ cards-example.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import nbayes
+import random
+
+class Card(nbayes.Instance):
+    def __init__(self, card_string):
+        labels = set()
+        if card_string[0] in 'JQK':
+            labels.add('face')
+            if card_string[0] == 'K':
+                labels.add('king')
+            if card_string[0] == 'J':
+                labels.add('jack')
+            if card_string[0] == 'Q':
+                labels.add('queen')
+        if card_string[0] == '2':
+            labels.add('deuce')
+        if card_string[0] == 'A':
+            labels.add('ace')
+        if card_string[1] in 'CS':
+            labels.add('black')
+            if card_string[1] == 'C':
+                labels.add('clubs')
+            if card_string[1] == 'S':
+                labels.add('spades')
+        if card_string[1] in 'DH':
+            labels.add('red')
+            if card_string[1] == 'D':
+                labels.add('diamonds')
+            if card_string[1] == 'H':
+                labels.add('hearts')
+        super(Card,self).__init__(card_string, *labels)
+
+c = nbayes.Classifier()
+c.add_instances(*[ Card(v+s) for s in 'CDHS' for v in '23456789TJQKA' ])
+
+# prob_lattr is prob label or attr
+p_king = c.prob_lattr('king')
+p_face = c.prob_lattr('face')
+p_face_given_king = c.prob_lattr_given_lattr('face','king')
+
+assert p_king == 4.0/52
+assert p_face == 12.0/52
+assert p_face_given_king == 1.0 # all kings are faces
+
+p_king_given_face = c.posterior('king','face') # P(H|E) = P(E|H)/P(E) * P(H)
+assert p_king_given_face == 1.0/3
+
+print("P(King)      = {:0.4f} aka the prior probability".format(p_king))
+print("P(Face)      = {:0.4f}".format(p_face))
+print("P(Face|King) = {:0.4f}".format(p_face_given_king))
+print("P(E|H)/P(E)  = {:0.4f} aka the likelyhood ratio".format(c.likelyhood_ratio('face','king')))
+print("P(King|Face) = P(Face|King)/P(Face) * P(King) aka the posterior probability")
+print("             = {:0.4f}/{:0.4f} * {:0.4f}".format(p_face_given_king, p_face, p_king))
+print("             = {:0.4f}".format( p_king_given_face ))
--- nbayes.py
+++ nbayes.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
+my_iterable = (tuple,list,set,dict)
+
 class Instance(object):
     def __init__(self, label, *attr):
         self.label = label
         self.attr  = set(attr)
+
+    @property
+    def lattr(self):
+        return set(self.label).union(self.attr)
 
 class Classifier(object):
     def __init__(self, *instances):
@@ -37,7 +43,7 @@ class Classifier(object):
 
     def prob_label(self,label): # P(A)
         if not self.corpus:
-            return 0
+            return 0.0
 
         c = 0.0
         for i in self.corpus:
@@ -47,7 +53,7 @@ class Classifier(object):
 
     def prob_attr(self,*attr): # P(B)
         if not self.corpus:
-            return 0
+            return 0.0
 
         c = 0.0
         for i in self.corpus:
@@ -72,3 +78,44 @@ class Classifier(object):
         if p_B == 0:
             return 0
         return (self.prob_attr_given_label(label,*attr) * self.prob_label(label)) / p_B
+
+
+
+    # more generalized P(H|E) = P(E|H)/P(E) * P(H)
+    # probability hypothesis is true given evidence
+    # is the prior probability P(H) times the likelyhood ratio P(E|H)/P(E)
+
+    def prob_lattr(self,*lattr): # P(H) or P(E)
+        if not self.corpus:
+            return 0.0
+
+        if len(lattr) == 1 and isinstance(lattr[0], my_iterable):
+            lattr = lattr[0]
+
+        c = 0.0
+        for i in self.corpus:
+            if i.lattr.issuperset(lattr) :
+                c += 1.0
+        return c / len(self.corpus)
+    prior = prob_lattr
+
+    def prob_lattr_given_lattr(self,eset,hset): # P(E|H)
+        if not isinstance(eset, my_iterable): eset = set([eset])
+        if not isinstance(hset, my_iterable): hset = set([hset])
+        ic = 0
+        ac = 0.0
+        for i in self.corpus:
+            l = i.lattr
+            if l.issuperset(hset):
+                ic += 1
+                if l.issuperset(eset):
+                    ac += 1.0
+        if ic == 0:
+            return 0
+        return ac / ic
+
+    def likelyhood_ratio(self, eset,hset): # P(E|H)/P(E)
+        return self.prob_lattr_given_lattr(eset,hset) / self.prob_lattr(eset)
+
+    def posterior(self,hset,eset):
+        return self.likelyhood_ratio(eset,hset) * self.prior(hset)
