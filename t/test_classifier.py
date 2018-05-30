b05aae12de59cebbb8463256fc80d1d909c02f21
Paul Miller <jettero@corky.vhb.voltar.org>
2018-05-30 11:02:35 -0400

    add some numerical tests so later revisions don't foob the computations unexpectedly

--- t/test_classifier.py
+++ t/test_classifier.py
@@ -6,9 +6,12 @@ def test_classifier(emails,email_data):
     ambig2 = set(['this', 'that', 'blah'])
     ambig3 = set(['this', 'that', 'the-other'])
 
+    def _p(x):
+        return emails.prob_label_not_label_given_attr('spam','ham', x)
+
     for ed in email_data:
         a = set(ed[1:])
-        p = emails.prob_label_not_label_given_attr('spam','ham', a)
+        p = _p(a)
 
         if ed[0] == 'spam':
             assert p > 0.5 or a == ambig1
@@ -23,3 +26,7 @@ def test_classifier(emails,email_data):
 
         ap = emails.prob_all_labels(a)
         assert ap['spam'] == p
+
+    assert _p(['this'])      == pytest.approx(1.0/2)
+    assert _p(['that'])      == pytest.approx(5.0/9)
+    assert _p(['the-other']) == pytest.approx(0.48387096774193544)
