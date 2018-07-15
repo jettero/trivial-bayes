0077bda026a42451682e21fe5bd2af723a44dcec
Paul Miller <jettero@corky.vhb.voltar.org>
2018-07-15 12:28:03 -0400

    classifier tables …

--- t/test_classifier.py
+++ t/test_classifier.py
@@ -1,5 +1,6 @@
 
 import pytest
+import random
 
 def test_classifier(emails,email_data):
     ambig1 = set(['this', 'the-other'])
@@ -30,3 +31,33 @@ def test_classifier(emails,email_data):
     assert _p(['this'])      == pytest.approx(1.0/2)
     assert _p(['that'])      == pytest.approx(5.0/9)
     assert _p(['the-other']) == pytest.approx(0.48387096774193544)
+
+def _word(l, alphabet='abcdefghijklmnopqrstuvwxyz'):
+    return ''.join([ random.choice(alphabet) for x in range(l) ])
+
+def test_big_table():
+    random.seed(0)
+    words = set()
+    while len(words) < 50:
+        words.add( _word(7) )
+    words = sorted(words)
+
+    corpus = {
+        'l1': words[0:5]   + words[25:],
+        'l2': words[5:10]  + words[25:],
+        'l4': words[7:13]  + words[25:],
+        'l3': words[10:15] + words[25:],
+    }
+
+    import nbayes
+    cl = nbayes.Classifier()
+    for k in corpus:
+        cl.add_instance(k, corpus[k])
+
+    for k in corpus:
+        t = cl.classify_table( corpus[k][0:5] )
+        assert list(t) == [k]
+
+    for k in corpus:
+        t = cl.classify_table( words )
+        assert list(t) == []
