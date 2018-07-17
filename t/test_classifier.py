81b8c439ecfdfd5097633e7a9eab766f470eed4e
Paul Miller <jettero@corky.vhb.voltar.org>
2018-07-17 09:39:01 -0400

    more test

--- t/test_classifier.py
+++ t/test_classifier.py
@@ -9,3 +9,6 @@ def test_classifier(emails,email_words):
 
     emails.default = 'works' # set this initializer -- Classifier(default='works')
     assert emails.classify( email_words['all']  ).final == 'works'
+
+    assert emails.classify( email_words['ham'] + email_words['spam'][0:1]  ).final == 'ham'
+    assert emails.classify( email_words['spam'] + email_words['ham'][0:1]  ).final == 'spam'
